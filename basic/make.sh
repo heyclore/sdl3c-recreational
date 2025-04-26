4a349b0d2ca5a853a893aad756db7a849a19d3c7
heyclore <cloore@gmail.com>
2025-04-27 00:41:44 +0700

    demo

--- basic/make.sh
+++ basic/make.sh
@@ -1,1 +1,5 @@
-gcc main.c dry/sdl_app.c -o exe.exe -I ~/apps/pac/sdl3/include/ -L ~/apps/pac/sdl3/lib/ -lSDL3
+main='demo.c'
+exe='exe.exe'
+
+gcc $main dry/sdl_app.c -o $exe -I ~/apps/pac/sdl3/include/ -L ~/apps/pac/sdl3/lib/ -lSDL3
+./$exe
